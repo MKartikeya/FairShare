f4b1cdcaef7f7157360ff616fa98df6dca37db93
ksananth4424 <ksananth4424@gmail.com>
2023-06-21 21:16:25 +0530

    additions

---
+++ events.php
@@ -0,0 +1,22 @@
+<?php
+$mysql = mysqli_connect("localhost","root","","FairShare");
+if(!$mysql){
+    echo "Connection failed";
+}
+
+// Retrieve the values sent from JavaScript
+$values = json_decode($_POST['participantsList']);
+
+$query = "TRUNCATE TABLE Participants";
+mysqli_query($mysql, $query);
+
+// Prepare and execute the INSERT statement for each value
+foreach ($values as $value) {
+  $value = mysqli_real_escape_string($mysql, $value); // Sanitize the value
+  $query = "INSERT INTO Participants (Name) VALUES ('$value')";
+  mysqli_query($mysql, $query);
+}
+
+// Close the database connection
+mysqli_close($mysql);
+?>
