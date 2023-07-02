df2dd182655c5d2f327da14a8b2083d43f532af0
nimaiParsa <nimaiparsa@gmail.com>
2023-07-02 15:02:05 +0530

    results

---
+++ results.js
@@ -0,0 +1,18 @@
+
+// =======================================================
+// nav-bar 
+
+// var navMenu = document.querySelector('.logo');
+// console.log(navMenu);
+// var menuClose = document.querySelector('.menu-close-btn');
+
+function openMenu() {
+  document.getElementsByClassName('nav-tab-container')[0].style.display = 'block';
+    console.log(0);
+}
+
+
+function closeMenu() {
+    document.getElementsByClassName('nav-tab-container')[0].style.display = 'none';
+}
+// ======================================================
